8ab44746aca3ed4898cf9ec58b47590e9ca59bb1
Ruslan Tsuman <r.ts@me.com>
2024-04-18 22:46:09 +0200

    routerLink)))isActive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,16 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet]
+    imports: [RouterOutlet, RouterLink, RouterLinkActive]
 })
 export class AppComponent {
   title = 'movies';
 
-  constructor(private router: Router) { }
+
   
-  routeMovies() {
-    this.router.navigate(['movies']);
-  }
-  routeFavorites() {
-    this.router.navigate(['favorites']);
-  }
-  routeHome() {
-    this.router.navigate(['']);
-  }
 }
